7fb27a54babb8ced496c81e1601c253228ec4a5c
fridolph <249121486@qq.com>
2017-03-29 16:43:54 +0800

    添加demo

--- 数据结构与算法JavaScript描述/第02章 数组/2.6 二维和多维数组/01创建二维数组.js
+++ 数据结构与算法JavaScript描述/第02章 数组/2.6 二维和多维数组/01创建二维数组.js
@@ -32,15 +32,15 @@ Array.matrix = function(numrows, numcols, initial) {
 var nums = Array.matrix(5, 5, 0);
 console.log(nums[1][1]); // 0
 
-var names = Array.matrix(3,3,"");
-names[1][2] = "Joe";
+var names = Array.matrix(3,3,'');
+names[1][2] = 'Joe';
 console.log(names[1][2]); // Joe
 
 // 还可以仅用一行代码就创建并且使用一组初始值来初始化一个二维数组：
 var grades = [ 
   [89, 77, 89], 
-  ["好", "神", "奇"], 
-  [{id: "001"}, {key: "null"}, {value: true}]
+  ['好', '神', '奇'], 
+  [{id: '001'}, {key: 'null'}, {value: true}]
 ];
 
 console.log(grades[2][2]);
