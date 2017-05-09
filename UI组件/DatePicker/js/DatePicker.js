e91743bd2e2eb7594fe7e3fd75db886d8d3fa61a
fridolph <249121486@qq.com>
2017-05-09 10:18:11 +0800

    datepicker组件~  完善

--- UI组件/DatePicker/js/DatePicker.js
+++ UI组件/DatePicker/js/DatePicker.js
@@ -1,67 +1,79 @@
 ;(function() {
-  var DatePicker = {};
+  
+  let datepicker = {};
 
-  DatePicker.getMonthData = function(year, month) {
-    // result中的每个元素为当前月份的日期
-    var retult = [];    
+  datepicker.getMonthData = function(year, month) {
+    // 定义一个空数组用于存储日期数
+    let result = [];
 
-    // 当月的第一天
-    var firstDay = new Date(year, month - 1, 1);
-    // 当月第一天是星期几
-    var firstDayWeekDay = firstDay.getDay();
-    // 上月的最后一天
-    var lastDayOfLastMonth = new Date(year, month - 1, 0);
-    // 上月最后一天的日期
-    var lastDateOfLastMonth = lastDayOfLastMonth.getDate();
-
-    var preMonthDayCount = firstDayWeekDay - 1;
-    // 当月最后天
-    var lastDay = new Date(year, month, 0);
-    // 当月最后天的日期
-    var lastDate = lastDay.getDate();
-    
     if (!year || !month) {
-      var today = new Date();
-      year = today.getFullYear();
-      month = today.getMonth() + 1;
+      // 如果没有传入年份和月份，则新声明一个today对象并获取到当前年份及对应月份
+      var today = new Date();      // 2017-5-8
+      year = today.getFullYear();  // 2017
+      month = today.getMonth() + 1;  // 4 月份从0算， 这里+1  所以为5
     }
 
-    if (firstDayWeekDay === 0) firstDayWeekDay = 7;
+    // 获取到当月的第一天
+    var firstDay = new Date(year, month - 1, 1);
+    // 获取当月第一天是星期几
+    var firstDayWeekDay = firstDay.getDay();
 
-    // 通过循环，获取当月的每一天
-    // 通常一月4周以上，所以平衡算取6周的数据
-    for (var i = 0; i < 7 * 6; i++) {
-      // 当月的日期的越界处理
+    if (firstDayWeekDay === 0) { 
+      // 将周日赋值为7
+      firstDayWeekDay === 7;
+    }
+
+    year = firstDay.getFullYear();
+    month = firstDay.getMonth() + 1;
+
+    // 上月的最后一天
+    var lastDayOfLastMonth = new Date(year, month - 1, 0);
+    var lastDateOfLastMonth = lastDayOfLastMonth.getDate();
+
+    // 在日历的第一行需要显示多少个少月的日期
+    var preMonthDayCount = firstDayWeekDay - 1;
+
+    // 当月的最后一天
+    var lastDay = new Date(year, month, 0);
+    // 当月最后一天的日期
+    var lastDate = lastDay.getDate();
+
+    // 一周7天，2月可能只有给4行，相反31天的月份可能出现6行
+    for (var i = 0; i < 7*6; i++) { 
+      // 当前是哪一天
       var date = i + 1 - preMonthDayCount;
-      // 正常的显示日期
+      // 用来显示的天数
       var showDate = date;
+      // thisMonth赋值为当月
       var thisMonth = month;
       
-      if (date <= 0) { // 上月的情况处理
+      if (date <= 0) { // 上一月
         thisMonth = month - 1;
         showDate = lastDateOfLastMonth + date;
-      } else if (date > lastDate) { // 下一月的情况处理
+      } else if (date > lastDate) { // 下一月
         thisMonth = month + 1;
         showDate = showDate - lastDate;
-
-      }
-      // 对thisMonth进行处理
-      if (thisMonth === 0) { // 上一年的12月
-        thisMonth = 12;
-      }
-      if (thisMonth === 13) { // 下一年的1月
-        thisMonth = 1;
       }
 
-      retult.push({
+      // 月份为0则为上一年的12月
+      if (thisMonth === 0) thisMonth = 12;
+      // 月份为13则为下一年的1月
+      if (thisMonth === 13) thisMonth = 1;
+
+      result.push({
         month: thisMonth,
         date: date,
         showDate: showDate
-      });
+      })
     }
 
-    return retult;
+    return {
+      year,
+      month,
+      days: result
+    };
   }
 
-  window.DatePicker = DatePicker;
-})()
+  window.datepicker = datepicker;
+
+})();
