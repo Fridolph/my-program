95cefa07de8273bdeb5c66c0d859011d88dfc6e2
fridolph <249121486@qq.com>
2017-05-11 11:36:13 +0800

    导航菜单~· js

--- JS_JQuery/点击导航菜单跳转到指定位置特效/js/main.js
+++ JS_JQuery/点击导航菜单跳转到指定位置特效/js/main.js
@@ -1,21 +1,22 @@
-jQuery(document).ready(function ($) {
+$(function() {
   var secondaryNav = $('.cd-secondary-nav'),
     secondaryNavTopPosition = secondaryNav.offset().top,
-    taglineOffesetTop = $('#cd-intro-tagline').offset().top + $('#cd-intro-tagline').height() + parseInt($('#cd-intro-tagline').css('paddingTop').replace('px', '')),
-    contentSections = $('.cd-section');
+    contentSections = $('.cd-section'),
+    taglineOffesetTop = $('#cd-intro-tagline').offset().top + 
+      $('#cd-intro-tagline').height() + 
+      parseInt($('#cd-intro-tagline').css('paddingTop').replace('px', ''));
+  
 
   $(window).on('scroll', function () {
-    //on desktop - assign a position fixed to logo and action button and move them outside the viewport
-    ($(window).scrollTop() > taglineOffesetTop) ? $('#cd-logo, .cd-btn').addClass('is-hidden'): $('#cd-logo, .cd-btn').removeClass('is-hidden');
+    // 当滚动高度大于 taglineOffesetTop ， 就将logo部分隐藏
+    $(window).scrollTop() > taglineOffesetTop ? 
+      $('#cd-logo, .cd-btn').addClass('is-hidden') : 
+      $('#cd-logo, .cd-btn').removeClass('is-hidden');
 
-    //on desktop - fix secondary navigation on scrolling
+    // 当滚动高度大于 secondaryNavTopPosition， 固定nav及之后的逻辑
     if ($(window).scrollTop() > secondaryNavTopPosition) {
-      //fix secondary navigation
       secondaryNav.addClass('is-fixed');
-      //push the .cd-main-content giving it a top-margin
-      $('.cd-main-content').addClass('has-top-margin');
-      //on Firefox CSS transition/animation fails when parent element changes position attribute
-      //so we to change secondary navigation childrens attributes after having changed its position value
+      $('.cd-main-content').addClass('has-top-margin');      
       setTimeout(function () {
         secondaryNav.addClass('animate-children');
         $('#cd-logo').addClass('slide-in');
@@ -31,43 +32,54 @@ jQuery(document).ready(function ($) {
       }, 50);
     }
 
-    //on desktop - update the active link in the secondary fixed navigation
+    // 调用方法
     updateSecondaryNavigation();
   });
 
+
+  // 该方法通过判断高度决定是否启动锚点
   function updateSecondaryNavigation() {
     contentSections.each(function () {
       var actual = $(this),
-        actualHeight = actual.height() + parseInt(actual.css('paddingTop').replace('px', '')) + parseInt(actual.css('paddingBottom').replace('px', '')),
-        actualAnchor = secondaryNav.find('a[href="#' + actual.attr('id') + '"]');
-      if ((actual.offset().top - secondaryNav.height() <= $(window).scrollTop()) && (actual.offset().top + actualHeight - secondaryNav.height() > $(window).scrollTop())) {
+        actualAnchor = secondaryNav.find('a[href="#' + actual.attr('id') + '"]'),
+        actualHeight = actual.height() + 
+          parseInt(actual.css('paddingTop').replace('px', '')) + 
+          parseInt(actual.css('paddingBottom').replace('px', ''));
+          
+      if (actual.offset().top - secondaryNav.height() <= $(window).scrollTop() && 
+        actual.offset().top + actualHeight - secondaryNav.height() > $(window).scrollTop()) {
+        
         actualAnchor.addClass('active');
+
       } else {
+
         actualAnchor.removeClass('active');
+
       }
     });
   }
 
-  //on mobile - open/close secondary navigation clicking/tapping the .cd-secondary-nav-trigger
+  // 移动端的兼容
   $('.cd-secondary-nav-trigger').on('click', function (event) {
     event.preventDefault();
     $(this).toggleClass('menu-is-open');
     secondaryNav.find('ul').toggleClass('is-visible');
   });
 
-  //smooth scrolling when clicking on the secondary navigation items
+  // 平滑滚动,点击二级导航项
   secondaryNav.find('ul a').on('click', function (event) {
     event.preventDefault();
     var target = $(this.hash);
     $('body,html').animate({
       'scrollTop': target.offset().top - secondaryNav.height() + 1
     }, 400);
-    //on mobile - close secondary navigation
+
+    // 在移动端，关闭二级导航
     $('.cd-secondary-nav-trigger').removeClass('menu-is-open');
     secondaryNav.find('ul').removeClass('is-visible');
   });
 
-  //on mobile - open/close primary navigation clicking/tapping the menu icon
+  // 在移动端 关闭主导航
   $('.cd-primary-nav').on('click', function (event) {
     if ($(event.target).is('.cd-primary-nav')) $(this).children('ul').toggleClass('is-visible');
   });
