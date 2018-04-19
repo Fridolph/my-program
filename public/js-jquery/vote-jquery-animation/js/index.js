8b2f421000dc59f7f06b943134b046a10d3e8d84
fridolph <249121486@qq.com>
2018-04-19 11:37:07 +0800

    项目大整理~

--- public/js-jquery/vote-jquery-animation/js/index.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function () {
-  jQuery('.skillbar').each(function () {
-    jQuery(this).find('.skillbar-bar').animate({
-      width: jQuery(this).attr('data-percent')
-    }, 2500);
-  });
-});
