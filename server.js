4da5d7288ec3ab3625e9e20b7687e0b676626fef
fridolph <249121486@qq.com>
2019-01-28 14:09:43 +0800

    my-program main文件修改，todo 后续会用koa把program页重新弄下

--- server.js
+++ server.js
@@ -4,19 +4,30 @@ const app = new Koa()
 const router = require('koa-router')()
 const koaSend = require('koa-send')
 const koaStatic = require('koa-static')
-const pageStr = require('./public/homeStr')
+const homeStr = require('./homeStr')
+const programStr = require('./programStr')
 
 const homeRouter = router.get('/', async (ctx, next) => {
-  ctx.body = pageStr
+  ctx.body = homeStr
+  next()
+})
+
+const mainRouter = router.get('/program', async (ctx, next) => {
+  ctx.body = programStr
+  next()
 })
 
 app.use(koaStatic(path.join(__dirname, 'public')))
 
-app.use(homeRouter.routes()).use(homeRouter.allowedMethods())
+app
+  .use(homeRouter.routes())
+  .use(mainRouter.routes())
 
 app.use(async (ctx, next) => {
   if (ctx.path === '/favicon.ico') {
-    await koaSend(ctx, '/favicon.ico', { root: path.join(__dirname, './') })
+    await koaSend(ctx, '/favicon.ico', {
+      root: path.join(__dirname, './')
+    })
   } else {
     await next()
   }
